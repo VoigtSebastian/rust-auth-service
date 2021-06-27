60eda546399ede222af9f04c00795a965a484a39
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2021-06-27 15:52:54 +0200

    Add integration test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use std::{env, fs::File, io::BufReader};
-
-use database_integration::utility::create_db_pool;
-
 use actix_web::{App, HttpServer};
+use database_integration::utility::create_db_pool;
 use rustls::{
     internal::pemfile::{certs, pkcs8_private_keys},
     NoClientAuth, ServerConfig,
 };
+use std::{env, fs::File, io::BufReader};
 
 mod configuration;
 mod pages;
@@ -59,4 +57,120 @@ async fn main() -> std::io::Result<()> {
     .bind_rustls(build_address().as_str(), config)?
     .run()
     .await
+}
+
+// ----------------------------------------------------------------------------
+// integration tests
+// ----------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{cookie::Cookie, test, App};
+    use rand::{distributions::Alphanumeric, thread_rng, Rng};
+    use serde::Serialize;
+    use sqlx::{postgres::PgRow, Row};
+
+    #[derive(Serialize)]
+    struct Credentials {
+        username: String,
+        password: String,
+    }
+
+    #[ignore = "Database necessary to run these tests"]
+    #[actix_rt::test]
+    async fn register_login_info_logout() {
+        dotenv::dotenv().ok();
+        // create database pool
+        let pool = create_db_pool()
+            .await
+            .expect("could not create database pool");
+
+        // Create app with standard configuration
+        let mut app = test::init_service(
+            App::new()
+                .configure(|c| configuration::website(c, &pool))
+                .configure(|c| configuration::user_config(c, &pool))
+                .configure(|c| configuration::admin_config(c, &pool)),
+        )
+        .await;
+
+        // Tests start here
+        let credentials = Credentials {
+            username: std::str::from_utf8(
+                &thread_rng()
+                    .sample_iter(Alphanumeric)
+                    .take(32)
+                    .collect::<Vec<_>>(),
+            )
+            .unwrap()
+            .to_string()
+            .to_lowercase(),
+            password: "asdfasdfasdfasdf".to_string(),
+        };
+
+        // register user
+        let register_req = test::TestRequest::post()
+            .set_form(&credentials)
+            .uri("/register")
+            .to_request();
+        let resp = test::call_service(&mut app, register_req).await;
+        assert!(resp.status().is_success());
+
+        // check that the database contains the newly created user
+        let user_id: i32 = sqlx::query("SELECT * FROM users WHERE username = $1;")
+            .bind(&credentials.username)
+            .map(|row: PgRow| row.try_get("user_id").unwrap())
+            .fetch_one(&pool)
+            .await
+            .unwrap();
+
+        // login user
+        let login_req = test::TestRequest::post()
+            .set_form(&credentials)
+            .uri("/login")
+            .to_request();
+        let resp = test::call_service(&mut app, login_req).await;
+        assert!(resp.status().is_redirection());
+
+        // check that the session has been created successfully
+        sqlx::query("SELECT * FROM sessions WHERE user_id = $1;")
+            .bind(&user_id)
+            .fetch_one(&pool)
+            .await
+            .unwrap();
+
+        // get user information
+        let id_cookie = resp
+            .response()
+            .cookies()
+            .filter(|c| c.name() == "id")
+            .collect::<Vec<Cookie>>()
+            .get(0)
+            .unwrap()
+            .to_owned();
+
+        // lookup user information page
+        let info_req = test::TestRequest::get()
+            .cookie(id_cookie.clone())
+            .uri("/")
+            .to_request();
+        let resp = test::call_service(&mut app, info_req).await;
+        assert!(resp.status().is_success());
+
+        // logout user
+        let info_req = test::TestRequest::post()
+            .cookie(id_cookie)
+            .uri("/logout")
+            .to_request();
+        let resp = test::call_service(&mut app, info_req).await;
+        assert!(resp.status().is_redirection());
+
+        // check that the session has been deleted successfully
+        assert!(sqlx::query("SELECT * FROM sessions WHERE user_id = $1;")
+            .bind(&user_id)
+            .fetch_one(&pool)
+            .await
+            .is_err());
+    }
 }
